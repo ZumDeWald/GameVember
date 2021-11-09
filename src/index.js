81de460735f7f7d7c2b9de255aa0dcf694020893
ZumDeWald <zumdewald@gmail.com>
2021-11-08 23:03:45 -0600

    Guy sprite can walk around

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Phaser from "phaser";
 import File1 from "./assets/File1.json";
 import TileSet from "./assets/tileSet.png";
-import Bug from "./assets/bug.png";
-import BugAtlas from "./assets/bug.json";
+import BitGuy from "./assets/SpriteGuy/BitGuy.png";
+import BitGuyAtlas from "./assets/SpriteGuy/BitGuy.json";
 
 class File1Scene extends Phaser.Scene {
   constructor() {
@@ -12,7 +12,7 @@ class File1Scene extends Phaser.Scene {
   preload() {
     this.load.tilemapTiledJSON("file1Map", File1);
     this.load.image("tileSet", TileSet);
-    this.load.atlas("playerbug", Bug, BugAtlas);
+    this.load.atlas("bitGuy", BitGuy, BitGuyAtlas);
   }
 
   create() {
@@ -27,10 +27,74 @@ class File1Scene extends Phaser.Scene {
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    // Bug
-    this.bug = this.physics.add.sprite(20, 20, "playerbug");
-    this.bug.setCollideWorldBounds(true);
-    this.physics.add.collider(this.bug, file1Walls);
+    // Guy
+    this.guy = this.physics.add.sprite(20, 20, "bitGuy");
+    this.guy.setCollideWorldBounds(true);
+    this.physics.add.collider(this.guy, file1Walls);
+
+    this.anims.create({
+      key: "guy_idle_up",
+      frames: [{ key: "bitGuy", frame: "SpriteGuy12" }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: "guy_walk_up",
+      frames: [
+        { key: "bitGuy", frame: "SpriteGuy13" },
+        { key: "bitGuy", frame: "SpriteGuy14" },
+        { key: "bitGuy", frame: "SpriteGuy15" },
+        { key: "bitGuy", frame: "SpriteGuy16" },
+      ],
+      frameRate: 8,
+    });
+
+    this.anims.create({
+      key: "guy_idle_down",
+      frames: [{ key: "bitGuy", frame: "SpriteGuy10" }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: "guy_walk_down",
+      frames: [
+        { key: "bitGuy", frame: "SpriteGuy1" },
+        { key: "bitGuy", frame: "SpriteGuy2" },
+        { key: "bitGuy", frame: "SpriteGuy3" },
+        { key: "bitGuy", frame: "SpriteGuy4" },
+      ],
+      frameRate: 8,
+    });
+
+    this.anims.create({
+      key: "guy_idle_left",
+      frames: [{ key: "bitGuy", frame: "SpriteGuy1" }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: "guy_walk_left",
+      frames: [
+        { key: "bitGuy", frame: "SpriteGuy12" },
+        { key: "bitGuy", frame: "SpriteGuy9" },
+        { key: "bitGuy", frame: "SpriteGuy10" },
+        { key: "bitGuy", frame: "SpriteGuy11" },
+      ],
+      frameRate: 8,
+    });
+
+    this.anims.create({
+      key: "guy_idle_right",
+      frames: [{ key: "bitGuy", frame: "SpriteGuy14" }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: "guy_walk_right",
+      frames: [
+        { key: "bitGuy", frame: "SpriteGuy5" },
+        { key: "bitGuy", frame: "SpriteGuy6" },
+        { key: "bitGuy", frame: "SpriteGuy7" },
+        { key: "bitGuy", frame: "SpriteGuy8" },
+      ],
+      frameRate: 8,
+    });
 
     this.cameras.main.setBounds(
       0,
@@ -38,24 +102,34 @@ class File1Scene extends Phaser.Scene {
       file1Map.widthInPixels,
       file1Map.heightInPixels
     );
-    this.cameras.main.startFollow(this.bug, true);
+    this.cameras.main.startFollow(this.guy, true);
   }
 
   update() {
-    if (this.cursors.left.isDown) {
-      this.bug.setVelocityX(-175);
+    if (this.cursors.up.isDown) {
+      this.guy.play("guy_walk_up", true);
+    } else if (this.cursors.down.isDown) {
+      this.guy.play("guy_walk_down", true);
+    } else if (this.cursors.left.isDown) {
+      this.guy.play("guy_walk_left", true);
     } else if (this.cursors.right.isDown) {
-      this.bug.setVelocityX(175);
-    } else {
-      this.bug.setVelocityX(0);
+      this.guy.play("guy_walk_right", true);
     }
 
     if (this.cursors.up.isDown) {
-      this.bug.setVelocityY(-175);
+      this.guy.setVelocityY(-175);
     } else if (this.cursors.down.isDown) {
-      this.bug.setVelocityY(175);
+      this.guy.setVelocityY(175);
     } else {
-      this.bug.setVelocityY(0);
+      this.guy.setVelocityY(0);
+    }
+
+    if (this.cursors.left.isDown) {
+      this.guy.setVelocityX(-175);
+    } else if (this.cursors.right.isDown) {
+      this.guy.setVelocityX(175);
+    } else {
+      this.guy.setVelocityX(0);
     }
   }
 }
