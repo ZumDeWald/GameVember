c914bcab002841dcd03f05aa653069c51fb226f7
ZumDeWald <zumdewald@gmail.com>
2021-11-10 23:10:02 -0600

    Setup basic platformer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Phaser from "phaser";
-import File1 from "./assets/File1.json";
-import TileSet from "./assets/tileSet.png";
-import Bug from "./assets/bug.png";
-import BugAtlas from "./assets/bug.json";
+import Level1 from "./assets/TestMap.json";
+import SpaceTiles from "./assets/spaceTiles.png";
+import Player from "./assets/player.png";
+import PlayerAtlas from "./assets/player.json";
 
 class File1Scene extends Phaser.Scene {
   constructor() {
@@ -10,52 +10,87 @@ class File1Scene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.tilemapTiledJSON("file1Map", File1);
-    this.load.image("tileSet", TileSet);
-    this.load.atlas("playerbug", Bug, BugAtlas);
+    this.load.tilemapTiledJSON("level1Map", Level1);
+    this.load.image("spaceTiles", SpaceTiles);
+    this.load.atlas("player", Player, PlayerAtlas);
   }
 
   create() {
     // Map
-    const file1Map = this.make.tilemap({ key: "file1Map" });
-    const tileSet = file1Map.addTilesetImage("tileSet", "tileSet");
-    file1Map.createStaticLayer("Background", tileSet, 0, 0);
-    this.physics.world.setBounds(0, 0, 800, 1600);
+    const level1Map = this.make.tilemap({ key: "level1Map" });
+    const spaceTiles = level1Map.addTilesetImage("spaceTiles");
+    level1Map.createLayer("background", spaceTiles, 0, 0);
+    this.physics.world.setBounds(0, 0, 800, 800);
 
-    const file1Walls = file1Map.createStaticLayer("Walls", tileSet, 0, 0);
-    file1Walls.setCollisionByExclusion(-1, true);
+    const level1Walls = level1Map.createLayer("platforms", spaceTiles, 0, 0);
+    level1Walls.setCollisionByExclusion(-1, true);
+
+    const level1foreground = level1Map.createLayer(
+      "foreground",
+      spaceTiles,
+      0,
+      0
+    );
+    level1foreground.setDepth(5);
+
+    const level1hurts = level1Map.createLayer("hurts", spaceTiles, 0, 0);
+    level1hurts.setDepth(4);
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    // Bug
-    this.bug = this.physics.add.sprite(20, 20, "playerbug");
-    this.bug.setCollideWorldBounds(true);
-    this.physics.add.collider(this.bug, file1Walls);
+    // Characters
+    this.player = this.physics.add.sprite(20, 20, "player");
+    this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, level1Walls);
 
     this.cameras.main.setBounds(
       0,
       0,
-      file1Map.widthInPixels,
-      file1Map.heightInPixels
+      level1Map.widthInPixels,
+      level1Map.heightInPixels
     );
-    this.cameras.main.startFollow(this.bug, true);
+    this.cameras.main.startFollow(this.player, true);
+
+    // Animations
+    this.anims.create({
+      key: "player_idle",
+      frames: this.anims.generateFrameNames("player", {
+        prefix: "player_idle",
+        start: 1,
+        end: 1,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "player_run",
+      frames: this.anims.generateFrameNames("player", {
+        prefix: "player_run",
+        start: 1,
+        end: 1,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
   }
 
   update() {
     if (this.cursors.left.isDown) {
-      this.bug.setVelocityX(-175);
+      this.player.setFlipX(true);
+      this.player.play("player_run", true);
+      this.player.setVelocityX(-175);
     } else if (this.cursors.right.isDown) {
-      this.bug.setVelocityX(175);
+      this.player.setFlipX(false);
+      this.player.play("player_run", true);
+      this.player.setVelocityX(175);
     } else {
-      this.bug.setVelocityX(0);
+      this.player.play("player_idle", true);
+      this.player.setVelocityX(0);
     }
 
-    if (this.cursors.up.isDown) {
-      this.bug.setVelocityY(-175);
-    } else if (this.cursors.down.isDown) {
-      this.bug.setVelocityY(175);
-    } else {
-      this.bug.setVelocityY(0);
+    if (this.cursors.up.isDown && this.player.body.onFloor()) {
+      this.player.setVelocityY(-300);
     }
   }
 }
@@ -69,7 +104,7 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: 0,
+      gravity: { y: 600 },
       debug: false,
     },
   },
